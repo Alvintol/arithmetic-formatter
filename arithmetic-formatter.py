5490caa518d49264d76709732f54c9f11ed39ddd
Alvin <alvin.tol@hotmail.com>
2022-09-05 19:21:45 -0600

    adds addition conditional to display equation answer

--- arithmetic-formatter.py
+++ arithmetic-formatter.py
@@ -1,9 +1,16 @@
+from ast import Num
+
+
 def formatter(list, answers = False) :
   answers = ''
   
   for item in list :
     splitItem = item.split()
-    print(f'{splitItem[0]}\n{splitItem[1]} {splitItem[2]}\n------\n{answers}')
+    
+    if splitItem[1] == '+' :
+      answers = int(splitItem[0]) + int(splitItem[2])
+    
+    print(f'{splitItem[0]}\n{splitItem[1]} {splitItem[2]}\n-------\n{answers}\n')
 
 formatter(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
 formatter(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
