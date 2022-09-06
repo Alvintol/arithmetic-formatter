dc32a52bdd7e7dbbec809a620bd6af16594a2d6b
Alvin <alvin.tol@hotmail.com>
2022-09-05 18:49:10 -0600

    adds formatter function that prints provided lidsyt

---
+++ arithmetic-formatter.py
@@ -0,0 +1,7 @@
+def formatter(list, answers = False) :
+  print(list)
+  
+formatter('test098')
+
+# formatter(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+# formatter(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
