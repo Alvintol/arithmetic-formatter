14c40ac487022f06b52bff801078f91f3c99854c
Alvin <alvin.tol@hotmail.com>
2022-09-05 19:09:38 -0600

    prints equation to e human readable

--- arithmetic-formatter.py
+++ arithmetic-formatter.py
@@ -1,7 +1,7 @@
 def formatter(list, answers = False) :
   for item in list :
     splitItem = item.split()
-    print(splitItem)
+    print(f'{splitItem[0]}\n{splitItem[1]} {splitItem[2]}\n------')
 
 formatter(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
 formatter(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
