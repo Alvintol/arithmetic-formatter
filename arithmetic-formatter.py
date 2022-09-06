021009749e1a1532d90e5c9519a96a3d8834548b
Alvin <alvin.tol@hotmail.com>
2022-09-05 19:02:03 -0600

    splits each item

--- arithmetic-formatter.py
+++ arithmetic-formatter.py
@@ -1,6 +1,7 @@
 def formatter(list, answers = False) :
   for item in list :
-    print(item)
+    splitItem = item.split()
+    print(splitItem)
 
 formatter(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
 formatter(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
