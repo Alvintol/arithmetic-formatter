ff763d160b834fddc19745290ab961636507af85
Alvin <alvin.tol@hotmail.com>
2022-09-05 19:00:10 -0600

    maps through provided list and prints each individual item

--- arithmetic-formatter.py
+++ arithmetic-formatter.py
@@ -1,7 +1,6 @@
 def formatter(list, answers = False) :
-  print(list)
-  
-formatter('test098')
+  for item in list :
+    print(item)
 
-# formatter(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
-# formatter(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+formatter(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+formatter(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
